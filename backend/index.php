c859d8a137dc34703670a65f700e9df25f777dd6
Elias Kotlyar <elias.kotlyar@gmail.com>
2018-03-03 01:03:01 +0100

    Text

--- backend/index.php
+++ backend/index.php
@@ -5,10 +5,34 @@ require __DIR__ . '/vendor/autoload.php';
 $imagine = new Imagine\Imagick\Imagine();
 // make an empty image (canvas) 120x160px
 
-$constant = 250;
 
-$path = __DIR__."/sample/1_crop.jpg";
+
+$path = __DIR__ . "/1_crop.jpg";
 $collage = $imagine->open($path);
+
+
+
+
+// starting coordinates (in pixels) for inserting the first image
+$x = 0;
+$y = 0;
+$constant = 175;
+
+foreach (glob(__DIR__ . '/sample/*.jpg') as $path) {
+    // open photo
+    $photo = $imagine->open($path);
+
+    $photo->resize(new Imagine\Image\Box($constant, $constant));
+
+
+    // paste photo at current position
+    $collage->paste($photo, new Imagine\Image\Point($x, $y));
+
+    // move position by 30px to the right
+    $x += $constant;
+
+
+}
 
 
 
